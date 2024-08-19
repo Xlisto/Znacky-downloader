59ee57f32b4e2a40025fe2f7aed7e8484be1543e
Xlisto <x.listo@seznam.cz>
2024-08-19 09:35:41 +0200

    Doplněny komentáře do třídy WebLoader.java

--- src/main/java/cz/xlisto/znacky/WebLoader.java
+++ src/main/java/cz/xlisto/znacky/WebLoader.java
@@ -10,11 +10,48 @@ import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * Třída WebLoader slouží k načítání obsahu webových stránek z daných URL adres.
+ * <p>
+ * Tato třída poskytuje metody pro načtení obsahu webové stránky a získání seznamu URL adres
+ * nalezených parserem. Používá knihovny pro zpracování HTTP požadavků a logování.
+ * <p>
+ * Hlavní funkce třídy:
+ * <ul>
+ *   <li>Načítání obsahu webové stránky z dané URL pomocí metody loadWeb.</li>
+ *   <li>Získání seznamu URL adres nalezených parserem pomocí metody getUrlList.</li>
+ * </ul>
+ * <p>
+ * Třída využívá následující knihovny:
+ * <ul>
+ *   <li>java.net.HttpURLConnection pro zpracování HTTP požadavků.</li>
+ *   <li>org.slf4j.Logger pro logování chyb a informací.</li>
+ *   <li>javafx.collections.ObservableList pro práci se seznamem URL adres.</li>
+ * </ul>
+ */
 public class WebLoader {
+
+    // Logger pro zaznamenávání chyb a informací během načítání webových stránek
     private static final Logger logger = LoggerFactory.getLogger(WebLoader.class);
     // Vytvoření objektu třídy ParserHTML
     private final ParserHTML parser = new ParserHTML();
 
+    /**
+     * Metoda pro načtení obsahu webové stránky z dané URL.
+     *
+     * @param url URL adresa webové stránky, kterou chceme načíst.
+     *            <p>
+     *            Metoda provede následující kroky:
+     *            <ol>
+     *                <li>Vytvoří objekt URL a otevře spojení.
+     *                <li>Nastaví metodu požadavku na GET.
+     *                <li>Načte odpověď z webové stránky pomocí BufferedReader.
+     *                <li>Obsah stránky uloží do StringBuilderu.
+     *                <li>Zavolá metodu parseHTML třídy ParserHTML s načteným obsahem stránky.
+     *                <li>Pokud metoda parseHTML vrátí další URL, rekurzivně zavolá loadWeb s touto URL.
+     *                <li>V případě chyby při načítání webu zapíše chybu do logu.
+     *            </ol>
+     */
     public void loadWeb(String url) {
         try {
             // Vytvoření objektu URL a otevření spojení
@@ -47,7 +84,11 @@ public class WebLoader {
 
     }
 
-    // Získá ObservableList URL adres z parseru
+    /**
+     * Metoda pro získání seznamu URL adres z parseru.
+     *
+     * @return ObservableList obsahující URL adresy, které byly nalezeny parserem.
+     */
     public ObservableList<String> getUrlList() {
         return parser.getUrlList();
     }
